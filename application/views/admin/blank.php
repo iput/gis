c30f46db627f7f1fcd6de5c767c61b5536c20048
mhfd <makhfudzamhari@gmail.com>
2017-04-30 14:06:08 +0700

    interface

--- application/views/admin/blank.php
+++ application/views/admin/blank.php
@@ -65,11 +65,12 @@
           </div>
         </div>
         <div class="box-body">
-          Berita
+          <div id="mapcanvas" style="height:500px; width:100%;">
+          </div>
         </div>
         <!-- /.box-body -->
         <div class="box-footer">
-          
+
         </div>
         <!-- /.box-footer-->
       </div>
@@ -79,3 +80,18 @@
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
+    <script >
+    function myMap() {
+    var myCenter = new google.maps.LatLng(-7.950884, 112.608102);
+    var mapProp = {center:myCenter,
+    	zoom:15,
+    	scrollwheel:false,
+    	draggable:false,
+    	mapTypeId:google.maps.MapTypeId.ROADMAP};
+    var map = new google.maps.Map(document.getElementById("mapcanvas"),mapProp);
+    var marker = new google.maps.Marker({position:myCenter});
+    marker.setMap(map);
+    }
+    </script>
+  </script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAJ1XPieE32_RzapVtgPa8KXDU9qeilbTE&callback=myMap"></script>
--- application/views/user/landing.php
+++ application/views/user/landing.php
@@ -5,7 +5,11 @@
 <script>
 function myMap() {
 var myCenter = new google.maps.LatLng(-7.950884, 112.608102);
-var mapProp = {center:myCenter, zoom:15, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
+var mapProp = {center:myCenter,
+	zoom:15,
+	scrollwheel:false,
+	draggable:false,
+	mapTypeId:google.maps.MapTypeId.ROADMAP};
 var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 var marker = new google.maps.Marker({position:myCenter});
 marker.setMap(map);
